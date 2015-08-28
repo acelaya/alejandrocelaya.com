0d2e01ecbbe93a8322f7b397bfbc882dbe593028
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-08-28 19:27:38 +0200

    Improved config by using env vars

--- config/config.php
+++ config/config.php
@@ -1,6 +1,25 @@
 <?php
 use Zend\Config\Factory;
+use Zend\Config\Writer\PhpArray;
 
-return Factory::fromFiles(
+$env = getenv('APP_ENV') ?: 'dev';
+$mergedConfigFile = __DIR__ . '/../data/cache/config_cache.php';
+
+// If in production and merged config exists, return it
+if ($env === 'pro' && is_file($mergedConfigFile)) {
+    return include $mergedConfigFile;
+}
+
+// Merge configuration files
+$mergedConfig = Factory::fromFiles(
     glob(__DIR__ . '/autoload/{,*.}{global,local}.php', GLOB_BRACE)
 );
+
+// If in production, cache merged config
+if ($env === 'pro') {
+    $writer = new PhpArray();
+    $writer->toFile($mergedConfigFile, $mergedConfig);
+}
+
+// Finally, Return the merged configuration
+return $mergedConfig;
--- public/index.php
+++ public/index.php
@@ -1,12 +1,26 @@
 <?php
 use Interop\Container\ContainerInterface;
 use Zend\Expressive\Application;
+use Dotenv\Dotenv;
 
 // Change to the project root, to simplify resolving paths
 chdir(dirname(__DIR__));
 
 // Setup autoloading
 require __DIR__ . '/../vendor/autoload.php';
+
+// Load environment variables
+if (class_exists(Dotenv::class)) {
+    $dotenv = new Dotenv(__DIR__ . '/..');
+    $dotenv->required('APP_ENV')->allowedValues(['pro', 'dev']);
+    $dotenv->load();
+}
+
+// Set error reporting
+if (getenv('APP_ENV') === 'dev') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
 
 /** @var ContainerInterface $container */
 $container = include __DIR__ . '/../config/container.php';
