4ea4bcfb6b5595ae0b05030f3117084551f13ffb
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-09-05 21:50:41 +0200

    Improved Acelaya\Twig\Extension tests

--- tests/Twig/Extension/UrlExtensionTest.php
+++ tests/Twig/Extension/UrlExtensionTest.php
@@ -4,6 +4,8 @@ namespace AcelayaTest\Website\Twig\Extension;
 use Acelaya\Website\Service\RouteAssembler;
 use Acelaya\Website\Twig\Extension\UrlExtension;
 use PHPUnit_Framework_TestCase as TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Zend\Expressive\Router\RouteResult;
 
 class UrlExtensionTest extends TestCase
 {
@@ -11,12 +13,16 @@ class UrlExtensionTest extends TestCase
      * @var UrlExtension
      */
     protected $extension;
+    /**
+     * @var ObjectProphecy
+     */
+    protected $routeAssemblerProphezy;
 
     public function setUp()
     {
-        $routeAssembler = $this->prophesize(RouteAssembler::class);
-        $routeAssembler->assembleUrl('home', [], [], false)->willReturn('/foo/bar');
-        $this->extension = new UrlExtension($routeAssembler->reveal());
+        $this->routeAssemblerProphezy = $this->prophesize(RouteAssembler::class);
+        $this->routeAssemblerProphezy->assembleUrl('home', [], [], false)->willReturn('/foo/bar');
+        $this->extension = new UrlExtension($this->routeAssemblerProphezy->reveal());
     }
 
     public function testGetFunctions()
@@ -30,5 +36,16 @@ class UrlExtensionTest extends TestCase
     public function testAssembleUrl()
     {
         $this->assertEquals('/foo/bar', $this->extension->assembleUrl('home'));
+    }
+
+    public function testGetCurrentRouteResult()
+    {
+        $this->routeAssemblerProphezy->getCurrentRouteResult()->willReturn(RouteResult::fromRouteMatch(
+            'home',
+            'HelloWorld',
+            []
+        ));
+        $result = $this->extension->getCurrentRouteResult();
+        $this->assertInstanceOf(RouteResult::class, $result);
     }
 }
