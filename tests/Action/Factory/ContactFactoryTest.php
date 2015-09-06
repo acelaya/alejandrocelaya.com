d957dc50fce62b0e659a4383d9a3c7d05adfba1e
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-09-06 10:14:00 +0200

    Improved test coverage

---
+++ tests/Action/Factory/ContactFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace AcelayaTest\Website\Action\Factory;
+
+use Acelaya\Website\Action\Contact;
+use Acelaya\Website\Action\Factory\ContactFactory;
+use Acelaya\Website\Service\ContactService;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Expressive\Template\TemplateInterface;
+use Zend\ServiceManager\Config;
+use Zend\ServiceManager\ServiceManager;
+
+class ContactFactoryTest extends TestCase
+{
+    /**
+     * @var ContactFactory
+     */
+    protected $factory;
+
+    public function setUp()
+    {
+        $this->factory = new ContactFactory();
+    }
+
+    public function testInvoke()
+    {
+        $sm = new ServiceManager(new Config([
+            'services' => [
+                TemplateInterface::class => $this->prophesize(TemplateInterface::class)->reveal(),
+                ContactService::class => $this->prophesize(ContactService::class)->reveal(),
+            ]
+        ]));
+        $instance = $this->factory->__invoke($sm);
+        $this->assertInstanceOf(Contact::class, $instance);
+    }
+}
