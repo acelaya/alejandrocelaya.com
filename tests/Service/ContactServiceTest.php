3ffde11fd2564a41f794f95abf6b66fe28fed97d
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-09-06 10:24:08 +0200

    Increased code coverage to 100% at this point

---
+++ tests/Service/ContactServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace AcelayaTest\Website\Service;
+
+use Acelaya\Website\Service\ContactService;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Expressive\Template\Twig;
+
+class ContactServiceTest extends TestCase
+{
+    /**
+     * @var ContactService
+     */
+    protected $service;
+    /**
+     * @var array
+     */
+    protected $messageData = [
+        'email' => 'alejandro@alejandrocelaya.com'
+    ];
+    /**
+     * @var array
+     */
+    protected $config = [
+        'subject' => 'foo',
+        'to' => 'alejandro@alejandrocelaya.com',
+        'from' => 'alejandro@alejandrocelaya.com',
+    ];
+
+    public function setUp()
+    {
+        $mailer = new \Swift_Mailer(new \Swift_NullTransport());
+        $renderer = new Twig(new \Twig_Environment(new \Twig_Loader_Array([
+            ContactService::TEMPLATE => '<p>{{ email }}</p>'
+        ])));
+
+        $this->service = new ContactService($mailer, $renderer, $this->config);
+    }
+
+    public function testSend()
+    {
+        $result = $this->service->send($this->messageData);
+        $this->assertTrue($result);
+    }
+}
---
+++ tests/Service/Factory/ContactServiceFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace AcelayaTest\Website\Service\Factory;
+
+use Acelaya\Website\Service\ContactService;
+use Acelaya\Website\Service\Factory\ContactServiceFactory;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Expressive\Template\TemplateInterface;
+use Zend\ServiceManager\Config;
+use Zend\ServiceManager\ServiceManager;
+
+class ContactServiceFactoryTest extends TestCase
+{
+    /**
+     * @var ContactServiceFactory
+     */
+    protected $factory;
+
+    public function setUp()
+    {
+        $this->factory = new ContactServiceFactory();
+    }
+
+    public function testInvoke()
+    {
+        $sm = new ServiceManager(new Config([
+            'services' => [
+                \Swift_Mailer::class => $this->prophesize(\Swift_Mailer::class)->reveal(),
+                TemplateInterface::class => $this->prophesize(TemplateInterface::class)->reveal(),
+                'config' => [
+                    'mail' => []
+                ],
+            ]
+        ]));
+        $instance = $this->factory->__invoke($sm);
+        $this->assertInstanceOf(ContactService::class, $instance);
+    }
+}
