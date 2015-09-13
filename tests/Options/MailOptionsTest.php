108fb50c54916926e8d8286b98208587c4398caa
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-09-13 11:21:32 +0200

    Added Acelaya\Website\Options tests

---
+++ tests/Options/MailOptionsTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace AcelayaTest\Website\Options;
+
+use Acelaya\Website\Options\MailOptions;
+use Acelaya\Website\Options\SmtpOptions;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class MailOptionsTest extends TestCase
+{
+    /**
+     * @var MailOptions
+     */
+    protected $options;
+
+    public function testData()
+    {
+        $data = [
+            'smtp' => [],
+            'from' => 'from',
+            'to' => 'to',
+            'subject' => 'subject'
+        ];
+        $this->options = new MailOptions($data);
+
+        $this->assertInstanceOf(SmtpOptions::class, $this->options->getSmtp());
+        $this->assertEquals($data['from'], $this->options->getFrom());
+        $this->assertEquals($data['to'], $this->options->getTo());
+        $this->assertEquals($data['subject'], $this->options->getSubject());
+    }
+}
