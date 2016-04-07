1eb4f5b8035216178902e6a44d74a35a1f7b8582
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-04-07 19:41:16 +0200

    Added use statement to prevent FQCN ussage

--- config/autoload/middleware_pipeline.global.php
+++ config/autoload/middleware_pipeline.global.php
@@ -1,14 +1,15 @@
 <?php
 use Acelaya\Website\Middleware\CacheMiddleware;
 use Acelaya\Website\Middleware\LanguageMiddleware;
+use Zend\Expressive\Container\ApplicationFactory;
 
 return [
 
     'middleware_pipeline' => [
         ['middleware' => CacheMiddleware::class],
         ['middleware' => LanguageMiddleware::class],
-        Zend\Expressive\Container\ApplicationFactory::ROUTING_MIDDLEWARE,
-        Zend\Expressive\Container\ApplicationFactory::DISPATCH_MIDDLEWARE,
+        ApplicationFactory::ROUTING_MIDDLEWARE,
+        ApplicationFactory::DISPATCH_MIDDLEWARE,
     ]
 
 ];
