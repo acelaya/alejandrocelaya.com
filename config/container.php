a57d89a84a22f517540b3eabdc2e6a906c3b9b23
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-06-16 22:00:18 +0200

    Added last null coalescing operator

--- config/container.php
+++ config/container.php
@@ -4,4 +4,4 @@ use Zend\ServiceManager\ServiceManager;
 // Create a ServiceManager from service_manager config and register the merged config as a service
 $config = include __DIR__ . '/config.php';
 $config['service_manager']['services']['config'] = $config;
-return new ServiceManager(isset($config['service_manager']) ? $config['service_manager'] : []);
+return new ServiceManager($config['service_manager'] ?? []);
