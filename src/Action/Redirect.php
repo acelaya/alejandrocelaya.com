db6682f56d47fbedb6768122428df1098399914b
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-09-30 11:43:28 +0200

    Created redirection from old sitemap to new one

---
+++ src/Action/Redirect.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Acelaya\Website\Action;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\RedirectResponse;
+
+class Redirect implements MiddlewareInterface
+{
+    /**
+     * Process an incoming server request and return a response, optionally delegating
+     * to the next middleware component to create the response.
+     *
+     * @param ServerRequestInterface $request
+     * @param DelegateInterface $delegate
+     *
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate)
+    {
+        $to = $request->getAttribute('to');
+        return new RedirectResponse($to, 301);
+    }
+}
