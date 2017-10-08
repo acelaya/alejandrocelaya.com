7a89134d65b08285a367b23fd311764dc9fa6bc6
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-10-08 12:43:52 +0200

    Created LoggerFactorytest

---
+++ tests/Factory/LoggerFactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace AcelayaTest\Website\Factory;
+
+use Acelaya\Website\Factory\LoggerFactory;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+use Zend\ServiceManager\ServiceManager;
+
+class LoggerFactoryTest extends TestCase
+{
+    /**
+     * @var LoggerFactory
+     */
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new LoggerFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function serviceIsProperlyCreated()
+    {
+        $instance = $this->factory->__invoke(new ServiceManager(['services' => [
+            'config' => [
+                'logger' => ['file' => tempnam(sys_get_temp_dir(), 'acelaya')],
+            ],
+        ]]), '');
+
+        $this->assertInstanceOf(Logger::class, $instance);
+    }
+}
