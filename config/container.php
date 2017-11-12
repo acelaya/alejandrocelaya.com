9468eb6c07ad1fd4ea7b2df1bfb0ae8d499b0992
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-11-12 09:43:20 +0100

    Replaced vlucas/phpdotenv by symfony/dotenv

--- config/container.php
+++ config/container.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-use Dotenv\Dotenv;
+use Symfony\Component\Dotenv\Dotenv;
 use Zend\ServiceManager\ServiceManager;
 
 // Change to the project root, to simplify resolving paths
@@ -12,9 +12,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Load environment variables
 if (class_exists(Dotenv::class)) {
-    $dotenv = new Dotenv(__DIR__ . '/..');
-    $dotenv->load();
-    $dotenv->required('APP_ENV')->allowedValues(['pro', 'dev']);
+    $dotenv = new Dotenv();
+    $dotenv->load(__DIR__ . '/../.env');
 }
 
 // Create a ServiceManager from service_manager config and register the merged config as a service
