8e962b33b1de3ae594c0166ec0ee4687dfc7445e
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-12-09 20:18:52 +0100

    Fixed tests

--- tests/Service/ContactServiceTest.php
+++ tests/Service/ContactServiceTest.php
@@ -3,45 +3,45 @@ declare(strict_types=1);
 
 namespace AcelayaTest\Website\Service;
 
-use Acelaya\Website\Options\MailOptions;
 use Acelaya\Website\Service\ContactService;
+use AcMailer\Model\Email;
+use AcMailer\Result\MailResult;
+use AcMailer\Service\MailServiceInterface;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
-use Zend\Expressive\Template\TemplateRendererInterface;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class ContactServiceTest extends TestCase
 {
+    /**
+     * @var array
+     */
+    private const MESSAGE_DATA = [
+        'email' => 'alejandro@alejandrocelaya.com',
+    ];
+
     /**
      * @var ContactService
      */
     protected $service;
     /**
-     * @var array
+     * @var ObjectProphecy
      */
-    protected $messageData = [
-        'email' => 'alejandro@alejandrocelaya.com',
-    ];
-    /**
-     * @var array
-     */
-    protected $config = [
-        'subject' => 'foo',
-        'to' => 'alejandro@alejandrocelaya.com',
-        'from' => 'alejandro@alejandrocelaya.com',
-    ];
+    protected $mailService;
 
     public function setUp()
     {
-        $mailer = new \Swift_Mailer(new \Swift_NullTransport());
-        $renderer = $this->prophesize(TemplateRendererInterface::class);
-        $renderer->render(ContactService::TEMPLATE, Argument::cetera())->willReturn('<p>{{ email }}</p>');
-
-        $this->service = new ContactService($mailer, $renderer->reveal(), new MailOptions($this->config));
+        $this->mailService = $this->prophesize(MailServiceInterface::class);
+        $this->service = new ContactService($this->mailService->reveal());
     }
 
     public function testSend()
     {
-        $result = $this->service->send($this->messageData);
+        $this->mailService->send(Argument::cetera())->willReturn(new MailResult(new Email()))
+                                                    ->shouldBeCalledTimes(1);
+
+        $result = $this->service->send(self::MESSAGE_DATA);
+
         $this->assertTrue($result);
     }
 }
